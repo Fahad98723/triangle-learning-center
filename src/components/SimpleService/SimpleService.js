658af20ea6357144929e060b81600a4a4ee52314
Fahad98723 <md.ravi63@gmail.com>
2021-10-04 23:49:15 +0600

    done

--- src/components/SimpleService/SimpleService.js
+++ src/components/SimpleService/SimpleService.js
@@ -3,18 +3,19 @@ import { Card, Col } from 'react-bootstrap';
 import './SimpleService.css'
 const SimpleService = (props) => {
     //home page service section 4 items
-    const {name,image,price} = props.service
+    const {name,image,price,details} = props.service
     return (
         <div>
             <Col className = 'my-4'>
             <Card>
                 <Card.Img className = 'card-image' variant="top" src={image} />
                 <Card.Body>
-                    <Card.Title>{name}</Card.Title>
+                    <Card.Title class='fs-4'>{name}</Card.Title>
                     <Card.Text>
-                    {price}
+                    {details}
                     </Card.Text>
-                    <button className= 'btn btn-success'>Enrole Now</button>
+                    <Card.Footer>{price}</Card.Footer>
+                    <button className= 'btn btn-success mt-3'>Enrole Now</button>
                 </Card.Body>
             </Card>
             </Col>
